5cb08c0f22adf778ea83e2ba6cfa472522ce975b
alysonnyang <alyson.n.yang@gmail.com>
2022-09-02 18:23:20 +0900

    movie-app via react.js
    first commit

---
+++ src/routes/Home.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <h2>
+                <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
+              </h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
